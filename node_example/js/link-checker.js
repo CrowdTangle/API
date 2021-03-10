e76fd60570b785db8f7923d5ec75d72a81971d6d
George Mayer <georgemayer@fb.com>
2021-03-10 11:26:39 -0500

    fix license

--- node_example/js/link-checker.js
+++ node_example/js/link-checker.js
@@ -1,3 +1,10 @@
+/**
+Copyright (c) Facebook, Inc. and its affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 var width = 1200;
 var height = 800;
 var padding = 1.5; // separation between same-color nodes
